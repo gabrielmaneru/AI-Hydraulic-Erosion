2fc1463e21c01af9b6a1b9d26ab1b7c144df4a9f
Gabi <r3dstone.gd29@gmail.com>
2019-06-11 15:09:56 +0200

    Start Pathfinding

---
+++ Assignment 2/Clean.bat
@@ -0,0 +1,11 @@
+DEL /F /S /Q /AH "Pathfinding\*.suo"
+DEL /F /S /Q "Pathfinding\*.sdf"
+DEL /F /S /Q "Pathfinding\*.opensdf"
+DEL /F /S /Q "Pathfinding\*.sdf"
+DEL /F /S /Q "Pathfinding\*.db"
+RMDIR /S /Q "Pathfinding\Debug"
+RMDIR /S /Q "Pathfinding\Release"
+RMDIR /S /Q "Pathfinding\ipch"
+RMDIR /S /Q "Pathfinding\.vs"
+DEL Pathfinding\TimingResultsLong.txt
+DEL Pathfinding\outcome.txt
